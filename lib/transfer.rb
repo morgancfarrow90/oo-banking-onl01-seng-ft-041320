939742a6c921af78d03a7b4fac0c8846181e0315
Morgan Farrow <morgancfarrow@gmail.com>
2020-05-02 13:09:40 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -22,7 +22,7 @@ def valid?
       elsif @status == "complete" 
       puts "Transaction was already executed" 
       
-    else valid? == false || @sender.balance < 0 
+    else self.valid? == false || @sender.balance < 0 
       @status = "rejected"
       return "Transaction rejected. Please check your account balance."
     end
